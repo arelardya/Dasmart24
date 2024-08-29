8ea62ee717ef4159fb477be78eda3cf10f5d1335
stevanniep <stevanniep12@gmail.com>
2024-08-29 21:55:54 +0700

    update Sign In Page with dummy account

--- public/script.js
+++ public/script.js
@@ -1,9 +1,23 @@
+function submitForm() {
+    const dummyUsername = 'user123';
+    const dummyPassword = 'password123';
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if (username === dummyUsername && password === dummyPassword) {
+        window.location.href = '#marketplace'; // Replace with the URL of the page you want to redirect to
+    } else {
+        document.getElementById('message').classList.remove('hidden');
+    }
+}
+
 function changeImage(x) {
     let image = document.getElementById("nextButton");
 
     if (x==1) {
-        image.src = "./assets/nextButton2.png";
+        image.src = "./assets/button/nextButton2.png";
     } if (x==2) {
-        image.src = "./assets/nextButton.png";
+        image.src = "./assets/button/nextButton.png";
     }
 }
